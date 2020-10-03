bf17451b4888a99319daf5f2225f1f5c28b37991
Hermann HOL <hermann.hol@ens.uvsq.fr>
2020-10-03 14:28:34 +0200

    toSting()

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,16 @@
 
 class Fraction{
-  
+
+  Integer num;
+  Integer denom;
+
+  public Fraction(int numerateur, int denominateur){
+    num = numerateur;
+    denom = denominateur;
+  }
+
+  @Override
+  public String toString(){
+    return num.toString() + "/" + denom.toString();
+  }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main{
 
   public static void main(String[] args) {
-    System.out.println("Hello");
+
+    Fraction f1 = new Fraction(2,3);
+
+    System.out.println("f1 : "+f1);
   }
 }
